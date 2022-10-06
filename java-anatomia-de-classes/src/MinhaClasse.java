f4fc68cdee71ef76724c60813d66e229361d3184
Murilo Solino <103014786+murilosolino@users.noreply.github.com>
2022-10-06 15:50:18 -0300

    primeiro commit (classe)

---
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,21 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+       System.out.println ("Olá mundo");
+       String primeiroNome = "Murilo";
+       String segundoNome = "Solino";
+   
+   
+       String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+       System.out.println (nomeCompleto);
+    
+    }
+   
+       public static String nomeCompleto(String primeiroNome, String segundoNome) {
+           return "Resultado do metodo" + primeiroNome.concat("").concat(segundoNome);
+           
+       }
+   
+   
+   }
+   
